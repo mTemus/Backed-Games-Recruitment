1f5551e7b863adf7c50dae6a3aa1bb7caceef6df
mTemus <35636856+mTemus@users.noreply.github.com>
2024-10-08 14:28:47 +0200

    Add input base

---
+++ Backed Games Recruitment Task/Assets/_RecruitmentTask/Scripts/Architecture/Input/InputManager.cs
@@ -0,0 +1,35 @@
+using Assets._RecruitmentTask.Scripts.Architecture.Events.Events;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets._RecruitmentTask.Scripts.Architecture.Input
+{
+    public class InputManager : MonoBehaviour
+    {
+        [Header("Input Events")]
+        [SerializeField] private ScriptableEventEmpty m_onInteractionStarted;
+
+        private PlayerInput m_playerInput = new PlayerInput();
+
+
+        private void Awake()
+        {
+            m_playerInput.PlayerAbilities.Interaction.started += OnInteractionStarted;
+        }
+
+        public void OnGameStart()
+        {
+            m_playerInput.PlayerAbilities.Enable();
+        }
+
+        public void OnGameEnd()
+        {
+            m_playerInput.PlayerAbilities.Disable();
+        }
+
+        private void OnInteractionStarted(InputAction.CallbackContext context)
+        {
+            m_onInteractionStarted.Invoke(default);
+        }
+    }
+}
---
+++ Backed Games Recruitment Task/Assets/_RecruitmentTask/Scripts/Architecture/Other/SubscriptableValue/ScriptableSubscriptableValue.cs
@@ -0,0 +1,13 @@
+using Assets.Repos.Codebase.Code.Core.SubscripableValue;
+using UnityEngine;
+
+namespace Assets._RecruitmentTask.Scripts.Architecture.Other.SubscriptableValue
+{
+    public abstract class ScriptableSubscriptableValue<T> : ScriptableObject
+    {
+        [SerializeField] 
+        private SubscriptableValue<T> m_value;
+
+        public SubscriptableValue<T> MValue => m_value;
+    }
+}
